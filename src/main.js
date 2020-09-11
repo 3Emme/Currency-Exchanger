0602fbd480523ee1ed70fe3b785471fb7c2f9e7a
3Emme <emmettbuentiempo@gmail.com>
2020-09-11 11:37:31 -0700

    add UI logic for creation of ExchangeProfile object given form input

--- src/main.js
+++ src/main.js
@@ -2,3 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { ExchangeProfile } from './exchange.js';
+
+$(document).ready(function() {
+
+  $('#exchange_request_form').submit(function(event) {
+      event.preventDefault();
+      const inputCurrency = $('#input_currency').val();
+      const inputAmount = $('#input_amount').val();
+      // clearFields();
+      $('#exchange_results').html("");
+      let exchange = new ExchangeProfile(inputCurrency,inputAmount)
+    })
+  });
