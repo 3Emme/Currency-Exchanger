dead9879d381b61e852c2404f1740b21f75a5a62
3Emme <emmettbuentiempo@gmail.com>
2020-09-11 14:56:43 -0700

    add ExchangeRateObject class, refactor ExchangeProfile class to take this into consideration, and pass test 3

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,19 +1,22 @@
 export class ExchangeProfile {
-  constructor(inputCurrency,inputAmount) {
-    this.USD = 1,
-    this.EUR = 0.8449,
+  constructor(inputCurrency,inputAmount,exchangeRateObject) {
+    this.USD = exchangeRateObject.USD,
+    this.EUR = exchangeRateObject.EUR,
     this.inputCurrency = inputCurrency,
     this.inputAmount = inputAmount,
     this.outputAmount = this.exchangeResults(this.inputCurrency,this.inputAmount)
   }
 
   exchangeResults(inputCurrency,inputAmount) { 
-  console.log(`inputCurrency: ${inputCurrency}, inputAmount: ${inputAmount}`);
-  console.log(`this.EUR: ${this.EUR}`);
   let exchangeValue = this[inputCurrency];
-  console.log(`exchangeValue: ${exchangeValue}`);
   let result = exchangeValue*inputAmount;
-  console.log(`result: ${result}`);
   return result;
+  }
+}
+
+export class ExchangeRateObject {
+  constructor() {
+    this.USD = 1,
+    this.EUR = 0.8449
   }
 }
