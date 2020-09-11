a5529bb07f5189e05db2a9c32fb587664515074f
3Emme <emmettbuentiempo@gmail.com>
2020-09-11 15:34:33 -0700

    refactor tests to more accurately test for desired function of test 3

--- __tests__/exchange.test.js
+++ __tests__/exchange.test.js
@@ -3,20 +3,20 @@ import { ExchangeProfile, ExchangeRateObject } from '../src/js/exchange.js';
 describe('ExchangeProfile', () => {
 
   test('Test 1 should create an ExchangeProfile object', () =>{    
-    let rate1 = new ExchangeRateObject;
+    let rate1 = {conversion_rates:{USD:1,EUR:0.8449}};
     let exchange1 = new ExchangeProfile("USD",10,rate1);
     expect(exchange1.inputAmount).toEqual(10)
   });
 
   test('Test 2 should determine the value of ExchangeProfile.outputAmount', () =>{    
-    let rate2 = new ExchangeRateObject;
+    let rate2 = {conversion_rates:{USD:1,EUR:0.8449}};
     let exchange2 = new ExchangeProfile("EUR",10,rate2);
     expect(exchange2.outputAmount).toEqual(8.449)
   });
 
   test('Test 3 should create an ExchangeRateObject object, which will determine the currency values of the ExchangeProfile', () =>{    
-    let rate3 = new ExchangeRateObject;
+    let rate3 = {conversion_rates:{USD:1,EUR:0.8449}};
     let exchange3 = new ExchangeProfile("EUR",10,rate3);
-    expect(exchange3.EUR).toEqual(rate3.EUR)
+    expect(exchange3.EUR).toEqual(rate3.conversion_rates.EUR)
   });
 });
