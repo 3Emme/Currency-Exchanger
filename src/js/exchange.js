334f59158b88dabe16fe1b3ba002d11efb6b7035
3Emme <emmettbuentiempo@gmail.com>
2020-09-11 12:46:56 -0700

    first pass at passing exchange.test test 2

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,11 +1,18 @@
 export class ExchangeProfile {
   constructor(inputCurrency,inputAmount) {
-    this.inputCurrency = inputCurrency
-    this.inputAmount = inputAmount
+    this.inputCurrency = inputCurrency,
+    this.inputAmount = inputAmount,
+    this.outputAmount = this.exchangeResults(this.inputCurrency,this.inputAmount),
+    this.USD = 1,
+    this.EUR = 0.8449
+  }
+
+  exchangeResults(inputCurrency,inputAmount) { 
+  console.log(`inputCurrency: ${inputCurrency}, inputAmount: ${inputAmount}`);
+  let exchangeValue = this[inputCurrency];
+  console.log(`exchangeValue: ${exchangeValue}`);
+  let result = exchangeValue*inputAmount;
+  console.log(`result: ${result}`);
+  return result;
   }
 }
-
-
-// function displayExchangeResults(exchangeInput) { 
-  
-//   }
