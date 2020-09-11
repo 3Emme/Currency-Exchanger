7c2284642862f3f22c94dfe557cb12631ab83943
3Emme <emmettbuentiempo@gmail.com>
2020-09-11 11:36:35 -0700

    add logic for ExchangeProfile object creation

---
+++ src/js/exchange.js
@@ -0,0 +1,11 @@
+export class ExchangeProfile {
+  constructor(inputCurrency,inputAmount) {
+    this.inputCurrency = inputCurrency
+    this.inputAmount = inputAmount
+  }
+}
+
+
+// function displayExchangeResults(exchangeInput) { 
+  
+//   }
