9f8a29bf64d41d6f354160ffe8733008e0d44ed6
3Emme <emmettbuentiempo@gmail.com>
2020-09-11 12:31:13 -0700

    write exchange test 2

--- __tests__/exchange.test.js
+++ __tests__/exchange.test.js
@@ -2,9 +2,15 @@ import { ExchangeProfile } from '../src/js/exchange.js';
 
 describe('ExchangeProfile', () => {
   let exchange1;
+  let exchange2;
 
   test('Test 1 should create an ExchangeProfile object', () =>{    
     exchange1 = new ExchangeProfile("USD",10);
     expect(exchange1.inputAmount).toEqual(10)
+  });
+
+  test('Test 2 should determine the value of ExchangeProfile.outputAmount', () =>{    
+    exchange2 = new ExchangeProfile("EUR",10);
+    expect(exchange2.outputAmount).toEqual(8.449)
   });
 });
