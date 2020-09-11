ab4c552749e63ebd59c2d9cd90e374798299f8e6
3Emme <emmettbuentiempo@gmail.com>
2020-09-11 14:55:23 -0700

    write test 3 and adjust previous tests to take new structure into consideration

--- __tests__/exchange.test.js
+++ __tests__/exchange.test.js
@@ -1,16 +1,22 @@
-import { ExchangeProfile } from '../src/js/exchange.js';
+import { ExchangeProfile, ExchangeRateObject } from '../src/js/exchange.js';
 
 describe('ExchangeProfile', () => {
-  let exchange1;
-  let exchange2;
 
   test('Test 1 should create an ExchangeProfile object', () =>{    
-    exchange1 = new ExchangeProfile("USD",10);
+    let rate1 = new ExchangeRateObject;
+    let exchange1 = new ExchangeProfile("USD",10,rate1);
     expect(exchange1.inputAmount).toEqual(10)
   });
 
   test('Test 2 should determine the value of ExchangeProfile.outputAmount', () =>{    
-    exchange2 = new ExchangeProfile("EUR",10);
+    let rate2 = new ExchangeRateObject;
+    let exchange2 = new ExchangeProfile("EUR",10,rate2);
     expect(exchange2.outputAmount).toEqual(8.449)
+  });
+
+  test('Test 3 should create an ExchangeRateObject object, which will determine the currency values of the ExchangeProfile', () =>{    
+    let rate3 = new ExchangeRateObject;
+    let exchange3 = new ExchangeProfile("EUR",10,rate3);
+    expect(exchange3.EUR).toEqual(rate3.EUR)
   });
 });
