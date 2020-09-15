64e9e7bd6a46d7f6f8fa391f81cd30efe465da72
3Emme <emmettbuentiempo@gmail.com>
2020-09-15 01:00:29 -0700

    add additional conversion properties to ExchangeProfile to facilitate their use from API pull

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -2,6 +2,10 @@ export class ExchangeProfile {
   constructor(inputCurrency,inputAmount,exchangeRateObject) {
     this.USD = exchangeRateObject.conversion_rates.USD,
     this.EUR = exchangeRateObject.conversion_rates.EUR,
+    this.CHF = exchangeRateObject.conversion_rates.CHF,
+    this.JPY = exchangeRateObject.conversion_rates.JPY,
+    this.GBP = exchangeRateObject.conversion_rates.GBP,
+    this.AUD = exchangeRateObject.conversion_rates.AUD,
     this.inputCurrency = inputCurrency,
     this.inputAmount = inputAmount,
     this.outputAmount = this.exchangeResults(this.inputCurrency,this.inputAmount)
