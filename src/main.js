dc1463d46ec5eb33640d8283b2e187dd960206dd
3Emme <emmettbuentiempo@gmail.com>
2020-09-15 00:02:44 -0700

    refactor to troubleshoot exchangeResults. Error was due to async placement of append as variable was not defined by the time it was run when not in a promise

--- src/main.js
+++ src/main.js
@@ -11,14 +11,22 @@ $(document).ready(function() {
       event.preventDefault();
       const inputCurrency = $('#input_currency').val();
       const inputAmount = $('#input_amount').val();
+      console.log(`inputCurrency: ${inputCurrency}`);
+      console.log(`inputAmount: ${inputAmount}`);
       let exchange1;
+      let rateTest;
       // clearFields();
       $('#exchange_results').html("");
       // let exchange = new ExchangeProfile(inputCurrency)
       ExchangeRateService.getRates()
       .then(function(rateResponse) {
+        rateTest = rateResponse;
+        console.log(`rateTest: ${rateTest}`)
         exchange1 = new ExchangeProfile(inputCurrency,inputAmount,rateResponse);
-      });
+        $('#exchange_results').append(exchange1.exchangeResults(inputCurrency,inputAmount));
       console.log(`exchange1: ${exchange1}`)
+      });
+      // $('#exchange_results').append(exchange1.exchangeResults(inputCurrency,inputAmount));
+      // console.log(`exchange1: ${exchange1}`)
     })
   });
